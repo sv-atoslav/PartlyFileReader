6ccbf55c34ea824c4ca930be8096cfa1e9b4d96e
sv7atoslav <sv7atoslav@ukr.net>
2022-01-03 19:29:59 +0200

    tool work well, but I must polish output

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import argparse
 from pathlib import Path
+from typing import NamedTuple
 
 parser = argparse.ArgumentParser(description='Tool for read only unsecure lines with context')
-parser.add_argument("-f", "--file", help="file, that will be proceseed")
-parser.add_argument("-e", "--epsilon", help="allowed difference between inputed line and current", default=2)
-parser.add_argument("-l", "--lines",
-                    help="centers of context, format:\n"
-                         "integers between dots, no spacebars, example:"
-                         "3,7,12,13,14,25 ")
+parser.add_argument(
+    "-f", "--file", type=Path, required=True,
+    help="file, that will be processed")
+parser.add_argument(
+    # https://en.wikipedia.org/wiki/Epsilon_(disambiguation)#Mathematics
+    "-e", "--epsilon", type=int, required=True,
+    help="allowed difference between inputted line and current", default=2)
+parser.add_argument(
+    "-l", "--lines", required=True,
+    help="centers of context, format:\n"
+         "integers between dots, no spacebars, example:"
+         "3,7,12,13,14,25 ")
 args = parser.parse_args()
 
 
@@ -16,35 +23,73 @@ def fatal_error(message: str):
     exit()
 
 
-def read_file_as_list_of_lines(path_to_file: Path) -> list[str]:
-    """ file can be very big, so, this operation can not be repeated """
+# this action can not be repeat by business logic, so, i decide insert it directly at main function
+# def read_file_as_list_of_lines(path_to_file: Path) -> list[str]:
+#     try:
+#         with open(path_to_file, "r") as fi:
+#             return fi.readlines()
+#     except FileNotFoundError:
+#         fatal_error(f"unreachable path {path_to_file} - file not found, return empty list")
+#     except UnicodeDecodeError as ude:
+#         fatal_error(f"{path_to_file} - unicode error: {ude}")
+
+
+class CodeCoordinate(NamedTuple):
+    # script called for 1 file at 1 launch, so, we don't need store "filename" value
+    number_of_line: int
+    content_of_line: str
+
+
+# noinspection PyUnboundLocalVariable
+def acceptable_values(input_pointers: list[int]) -> list[str]:
+    """ return content of file, if all values are correct, else stop execution of program """
+    if args.epsilon < 0:
+        fatal_error("epsilon must be positive int")
     try:
+        path_to_file = args.file
         with open(path_to_file, "r") as fi:
-            return fi.readlines()
+            unsorted_lines = fi.readlines()
     except FileNotFoundError:
         fatal_error(f"unreachable path {path_to_file} - file not found, return empty list")
     except UnicodeDecodeError as ude:
         fatal_error(f"{path_to_file} - unicode error: {ude}")
+    file_size = len(unsorted_lines)
+    for not_shadow_number in input_pointers:  # PyShadowingNames
+        if not isinstance(not_shadow_number, int) or not_shadow_number < 1:
+            fatal_error(f"wrong value: {not_shadow_number}")
+        if not_shadow_number > file_size:
+            print(f"W: have a pointer to {not_shadow_number}th line, but file have only {file_size} lines")
+            input_pointers.remove(not_shadow_number)
+    return unsorted_lines
 
 
-def acceptable_values(whole_file: list[str], input_pointers: list[int]) -> bool:
-    if args.epsilon is not int or args.epsilon < 0:
-        fatal_error("epsilon must be positive int")
-    file_size = len(whole_file)
-    for number in input_pointers:
-        if number is not int or number < 1:
-            fatal_error(f"wrong value: {number}")
-        if number > file_size:
-            print(f"W: have a pointer to {number}th line, but file have only {file_size} lines")
-            input_pointers.remove(number)
-    return bool(file_size > 0)
+def draw_file(lines: list[str], pointers: list[int], dispersion: int) -> list[CodeCoordinate]:
+    """ used for show only lines, that "neighbours" to code with errors
+    :param dispersion: "radius" for pieces, that will be exported
+    :param pointers: list of "centers" of pieces
+    :param lines: collection of strings, that already was read from file
+    """
+    whole_file = [""] + lines  # used for human-friendly pointers
+    lines_to_show = []
+    for index in range(1, len(whole_file)):
+        for target in pointers:
+            if abs(target - index) <= dispersion:
+                lines_to_show.append(CodeCoordinate(
+                    number_of_line=index,
+                    content_of_line=whole_file[index]))
+                break  # if line (N) already append, skip and check line N+1
+    return list(set(lines_to_show))
 
 
 try:
-    pointers: list[int] = args.lines.split(",")
-    lines_to_filter = read_file_as_list_of_lines(args.file)
-    if not acceptable_values(lines_to_filter, pointers):
-        exit()
+    input_pointers: list[int] = []
+    for number in args.lines.split(","):
+        input_pointers.append(int(number))
+    filtered_values = draw_file(
+        acceptable_values(input_pointers),
+        input_pointers,
+        args.epsilon)
+    print(filtered_values)
 
 
 except Exception as e:
